85b2f6f9d0ae8858b1f4079dc297d56c53a9cc57
Wiktor Mazur <wiktormazur1@gmail.com>
2020-11-11 17:51:22 +0100

    Initial commit

---
+++ precache-manifest.b313612da21fc32938c3c218db01e3be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7aa6dc8c6d3a032821b950ec03d5df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "207fd4506a2f1849b09a",
+    "url": "/static/js/2.bbf1f378.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bbf1f378.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0adc40c84a8232e0e71",
+    "url": "/static/js/main.59b2dc87.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
